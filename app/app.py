38c8474ebbe92f3f50751fdbc0c73016fa95564b
San <ssandeshupretii@gmail.com>
2025-06-12 19:21:12 +0545

    Completed

--- app/app.py
+++ app/app.py
@@ -1,12 +1,29 @@
+import string
+import pandas as pd
+from nltk.corpus import stopwords
+import joblib
 from flask import Flask,render_template,request
 app = Flask(__name__)
 
 def preprocess(text):
-    return ''
+    text= text.lower() #lowercasin
+    text = text.translate(str.maketrans('', '', string.punctuation)) 
+
+    stopwords_en = stopwords.words("english")
+    words = text.split() #tokenization
+    filtered_words = [word for word in words if word not in stopwords_en]  # Remove stopwords
+    
+    vectorizer = joblib.load('/home/san/Desktop/spam-detection/models/vectorizer.pkl')
+    text_input = ' '.join(filtered_words)
+
+    vector = vectorizer.transform([text_input])
+    return vector
 
 
 def predict(processed_text):
-    return ''
+    model = joblib.load('/home/san/Desktop/spam-detection/models/spam_classifier.pkl')
+    prediction = model.predict(processed_text)
+    return "Spam" if prediction[0] == 1 else "Ham"
 
 @app.route('/')
 def index():
