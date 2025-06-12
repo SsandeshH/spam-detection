941a593b3e9e3cfee0d406dc03aadc81c56ab655
San <ssandeshupretii@gmail.com>
2025-06-12 16:48:16 +0545

    better templates, functions remaining

--- app/app.py
+++ app/app.py
@@ -1,10 +1,25 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
 app = Flask(__name__)
+
+def preprocess(text):
+    return ''
+
+
+def predict(processed_text):
+    return ''
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/submit',methods = ['POST'])
+def submit():
+    result = ''
+    if request.method == 'POST':
+        text = request.form.get('text')
+        processed_text = preprocess(text)
+        result = predict(processed_text)
+        return(render_template('index.html',result = result))
 
 
 if __name__ == "__main__":
